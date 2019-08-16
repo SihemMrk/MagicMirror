2050d7c2d7ca3470a32c1a6a1a3c4d132ea54bb7
Sihem Meriki <sihemmeriki2@gmail.com>
2019-08-16 15:34:44 +0200

    gpio in electron

--- index.js
+++ index.js
@@ -134,28 +134,16 @@ ipcMain.on("wait-for-data", (event, arg) => {
   musicsList();
 });
 
-function checkPlatform() {
-  var nodeListButton = document.getElementsByTagName("button").length;
-
-  if (os.platform() === "darwin") {
-    for (var i = 0; i < nodeListButton; i++) {
-      document.getElementsByTagName("button")[i].style.display = "block";
-    }
-  } else {
-    const Gpio = require("onoff").Gpio;
-    const pushButton = new Gpio(17, "in", "both");
-    pushButton.watch(function(err, value) {
-      if (err) {
-        console.error("There was an error", err);
-        return;
-      }
-      console.log(value);
-    });
-    function unexportOnClose() {
-      pushButton.unexport();
-    }
-    process.on(unexportOnClose);
+const Gpio = require("onoff").Gpio;
+const pushButton = new Gpio(17, "in", "both");
+pushButton.watch(function(err, value) {
+  if (err) {
+    console.error("There was an error", err);
+    return;
   }
+  console.log(value);
+});
+function unexportOnClose() {
+  pushButton.unexport();
 }
-
-checkPlatform();
+process.on(unexportOnClose);
